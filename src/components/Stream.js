29063bccb23954031b5ca2f0f5427569996044e3
alexKwonIsAwesome <khwalex@gmail.com>
2017-03-12 16:05:53 +0900

    init testings

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,36 +1,17 @@
 import React from 'react';
 
-class Stream extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    const { tracks = [] } = this.props;
-    return (
-      <div>
-        {
-          tracks.map((track, key) => {
-            return (
-              <div className="track" key={key}>
-                {track.title}
-                <button onClick={() => this.setState({[key] : !this.state[key]})} type="button">
-                  {this.state[key] ? "Dislike" : "Like"}
-                </button>
-              </div>
-            );
-          })
-        }
-      </div> 
-    );
-  }
-
-  componentWillMount() {
-  }
-
-  componentDidUpdate() {
-  }
-}
+const Stream = function ( tracks = [] ) {
+  return (
+    <div>
+      {
+        tracks.map((track, key) => {
+          return (
+            <div className="track" key={key}>{track.title}</div>
+          );
+        })
+      }
+    </div>
+  );
+};
 
 export default Stream;
