5b3b15bae5302b065a77ad528bdd20edd462b837
alexKwonIsAwesome <khwalex@gmail.com>
2017-03-12 17:36:02 +0900

    connected redux store to react:
    This is achieved by providing store through <Provider> to all of the
    react components. The connect method enables this to literally connect
    the store to the component inside of it.

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Stream = function ( tracks = [] ) {
+const Stream = function ({ tracks = [] }) {
   return (
     <div>
       {
         tracks.map((track, key) => {
-          return (
-            <div className="track" key={key}>{track.title}</div>
-          );
+          return <div className="track" key={key}>{track.title}</div>;
         })
       }
     </div>
   );
 };
 
-export default Stream;
+const mapStateToProps = function (state) {
+  return {
+    tracks: state.track
+  }
+}
+
+export default connect(mapStateToProps)(Stream);
